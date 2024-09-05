110f56201242430d985b04a2fa74db5d06bb0910
Guillermo González <guillermogcordero@gmail.com>
2024-09-05 13:11:26 +0200

    Update constants.py

--- src/constants.py
+++ src/constants.py
@@ -1,4 +1,4 @@
 import os
 
 IMAGE_SIZE = (128, 128)
-MODEL_PATH = "/Users/AIR/Desktop/DocsGuille/PyProjects/image_office_classifier/my_cnn_model.keras"
+MODEL_PATH = "/path/to/the/model/my_cnn_model.keras"
